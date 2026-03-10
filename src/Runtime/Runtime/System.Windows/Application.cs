162da43df1eb5b2cbf5a4c6b93dd601a123f1623
Andrey Arekhva <a.arekhva@gmail.com>
2026-03-10 17:08:07 +0300

    Add Application contructor for Xaml Designer

--- src/Runtime/Runtime/System.Windows/Application.cs
+++ src/Runtime/Runtime/System.Windows/Application.cs
@@ -89,6 +89,14 @@ namespace System.Windows
             Dispatcher.CurrentDispatcher.InvokeAsync(DoStartup);
         }
 
+        protected Application(XamlDesignerConstructorStub _)
+        {
+            if (this is IComponentConnector componentConnector)
+            {
+                componentConnector.InitializeComponent();
+            }
+        }
+
         /// <summary>
         /// Gets or sets the <see cref="OpenSilver.Theming.Theme"/> used across this <see cref="Application"/>.
         /// </summary>
